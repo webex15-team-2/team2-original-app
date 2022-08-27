75e290ff142c35c1590c76c3beb18a7deaf97487
KotaOmatsu <okota1218@icloud.com>
2022-08-27 19:22:26 +0900

    lint error解決

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,8 +14,8 @@ const firebaseConfig = {
   messagingSenderId: "616147935493",
   appId: "1:616147935493:web:c97782315663cbed40d76c",
   measurementId: "G-073Q4CGSJR",
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+getAnalytics(app)
