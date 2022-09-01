585d80e664e4ebe57735dc4f2d5f2b9c252c8dc0
Tomsoya8810 <soyan8810@gmail.com>
2022-09-01 13:22:17 +0900

    保存機能を作成しました。

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <h2>企業名</h2>
+  <input type="text" v-model="company" />
+  <h2>テーマ</h2>
+  <input type="text" v-model="esTheme" />
+  <h2>字数制限</h2>
+  <input type="number" v-model="limit" />
+  <h2>ES</h2>
+  <textarea cols="30" rows="10" v-model="esContent"></textarea>
+  <button v-on:click="post">保存</button>
 </template>
+
+<script>
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "@/firebase.js";
+
+export default {
+  data() {
+    return {
+      company: "",
+      esTheme: "",
+      limit: "",
+      esContent: "",
+      userId: "",
+    };
+  },
+  methods: {
+    async post() {
+      const esData = {
+        kigyou: this.company,
+        theme: this.esTheme,
+        seigen: this.limit,
+        es: this.esContent,
+      };
+      this.userId = "user2";
+      // ここにユーザIDが入る
+
+      await setDoc(doc(db, "users", this.userId), esData).then(() => {
+        this.newData.push(esData);
+      });
+      // usersコレクションの名前がユーザIDのドキュメントにデータが保存される
+
+      this.company = this.esTheme = this.limit = this.esContent = "";
+      // 保存後にボックスは空になる
+    },
+  },
+};
+</script>
 
 <style>
 #app {
